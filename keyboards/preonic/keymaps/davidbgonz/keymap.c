cfc4398fe28e952905fa42c70e131ac4d4a60133
David <david.gonz789@gmail.com>
2021-07-09 10:20:52 -0400

    Remobe Z left shift, added right ctl whel " key is held down

--- keyboards/preonic/keymaps/davidbgonz/keymap.c
+++ keyboards/preonic/keymaps/davidbgonz/keymap.c
@@ -56,17 +56,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Sh/En |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|LSft/Z|   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |RSft//|  "   |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |RSft//|RCtl/"|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Func | Caps | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_ESC,   KC_1,         KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,            KC_BSPC,   \
-  KC_TAB,   KC_Q,         KC_W ,   KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,            KC_DEL,    \
-  KC_LCTL,  KC_A,         KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,         KC_SFTENT, \
-  KC_LSFT,  LSFT_T(KC_Z), KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_QUOT,   \
-  FUNCTION, KC_CAPS,      KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,           KC_RGHT    \
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,            KC_BSPC,         \
+  KC_TAB,   KC_Q,    KC_W ,   KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,            KC_DEL,          \
+  KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,         KC_SFTENT,       \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), RCTL_T(KC_QUOT), \
+  FUNCTION, KC_CAPS, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,           KC_RGHT          \
 ),
 
 /* Colemak DH
@@ -77,17 +77,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |Sh/En |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|LSft/Z|   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |RSft//|  "   |
+ * | Shift|   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |RSft//|  "   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Func | Caps | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK_DH] = LAYOUT_preonic_grid( \
-  KC_ESC,   KC_1,         KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,            KC_BSPC,   \
-  KC_TAB,   KC_Q,         KC_W,    KC_F,    KC_P,  KC_B,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN,         KC_DEL,    \
-  KC_LCTL,  KC_A,         KC_R,    KC_S,    KC_T,  KC_G,   KC_M,   KC_N,  KC_E,    KC_I,    KC_O,            KC_SFTENT, \
-  KC_LSFT,  LSFT_T(KC_Z), KC_X,    KC_C,    KC_D,  KC_V,   KC_K,   KC_H,  KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_QUOT,   \
-  FUNCTION, KC_CAPS,      KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,           KC_RGHT    \
+  KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,            KC_BSPC,         \
+  KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,  KC_B,   KC_J,   KC_L,  KC_U,    KC_Y,    KC_SCLN,         KC_DEL,          \
+  KC_LCTL,  KC_A,    KC_R,    KC_S,    KC_T,  KC_G,   KC_M,   KC_N,  KC_E,    KC_I,    KC_O,            KC_SFTENT,       \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_D,  KC_V,   KC_K,   KC_H,  KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), RCTL_T(KC_QUOT), \
+  FUNCTION, KC_CAPS, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,           KC_RGHT          \
 ),
 
 /* Lower
